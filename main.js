dbacb01aac33191162f27f92f46e904d0c96ff08
dev4anand <iamanand1702@gmail.com>
2024-11-21 18:32:16 +0530

    modal changes

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-// $(document).ready(function() {
-//     $('#exampleModal').modal('show');
+$(document).ready(function() {
+    $('#exampleModal').modal('show');
 
-//     // Hide the modal after 5 seconds (5000 milliseconds)
-//     // setTimeout(function() {
-//       $('#exampleModal').modal('hide');
-//     // }, 2000);
-//   });
+    // Hide the modal after 5 seconds (5000 milliseconds)
+    // setTimeout(function() {
+      $('#exampleModal').modal('hide');
+    // }, 2000);
+  });
